8d25f46db44580e24ff437432e6079b64eec88de
anurag mohod <contntsols@gmail.com>
2016-11-13 04:11:41 +0530

    add phonenumber to the game

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-var jsonData = {
+var jsonData =  {
+
     "firstName": "John",
 
-    "lastName": "Smith"
+    "lastName": "Smith",
+
+    "isAlive": true,
+
+    "age": 25,
+
+    "address": {
+
+        "streetAddress": "21 2nd Street",
+
+        "city": "New York",
+
+        "state": "NY",
+
+        "postalCode": "10021-3100"
+
+    },
+
+    "phoneNumbers": [
+
+        {
+
+            "type": "home",
+
+            "number": "212 555-1234"
+
+        },
+
+        {
+
+            "type": "office",
+
+            "number": "646 555-4567"
+
+        }
+
+    ],
+
+    "children": [],
+
+    "spouse": null
+
 }
 
+console.log(jsonData);
 var output = document.getElementById('output');
 
-output.innerHTML = jsonData.firstName + ' ' + jsonData.lastName;
+output.innerHTML = jsonData.firstName + ' ' + jsonData.lastName + ' ' + jsonData.phoneNumbers[1].type;
 
