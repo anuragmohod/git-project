0906c3193c8223eabe02171c362fcdff168e446e
anurag mohod <contntsols@gmail.com>
2016-11-13 03:53:37 +0530

    fetch json data into html page

---
+++ script.js
@@ -0,0 +1,8 @@
+var jsonData = {
+    "firstName": "John",
+
+    "lastName": "Smith"
+}
+
+var output = document.getElementById('output');
+output.innerHTML = jsonData.firstName + ' ' + jsonData.lastName;
