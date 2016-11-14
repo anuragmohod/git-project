67fb9b8e6a38c15c9fc820eb3af17e365ad2a9c7
anurag mohod <contntsols@gmail.com>
2016-11-14 21:55:06 +0530

    fetch data from myjson api

--- script.js
+++ script.js
@@ -100,4 +100,20 @@ user2: jsonData2}
 console.log(users);
 var output = document.getElementById('output');
 
-output.innerHTML = 'nothing';
+var ajaxCall = new  XMLHttpRequest();
+
+var url = 'https://api.myjson.com/bins/2w85g';
+
+ajaxCall.open("GET", url, true);
+ajaxCall.setRequestHeader("content-type",'applciation/json');
+ajaxCall.onreadystatechange = function () {
+	'use strict';
+    if (ajaxCall.readyState === 4 && ajaxCall.status === 200){
+           var jcontent = JSON.parse(ajaxCall.responseText);
+        output.innerHTML = jcontent.firstName+ ' '+jcontent.lastName + ' '+ jcontent.address.city + ' ' + jcontent.phoneNumbers[0].number;
+        console.log(jcontent);
+    }
+};
+ajaxCall.send(null);
+
+
