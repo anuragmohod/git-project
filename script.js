ef5322859f4af819d5c008c39f6c9c73824e9cfe
anurag mohod <contntsols@gmail.com>
2016-11-14 18:38:11 +0530

    create users group. add another user

--- script.js
+++ script.js
@@ -46,8 +46,59 @@ var jsonData =  {
 
 };
 
-console.log(jsonData);
+var jsonData2 =  {
+
+    "firstName": "Mike",
+
+    "lastName": "pica",
+
+    "isAlive": true,
+
+    "age": 22,
+
+    "address": {
+
+        "streetAddress": "21 2nd Street",
+
+        "city": "New York",
+
+        "state": "NY",
+
+        "postalCode": "10021-3100"
+
+    },
+
+    "phoneNumbers": [
+
+        {
+
+            "type": "home",
+
+            "number": "212 555-1234"
+
+        },
+
+        {
+
+            "type": "office",
+
+            "number": "646 555-4567"
+
+        }
+
+    ],
+
+    "children": [],
+
+    "spouse": null
+
+};
+
+var users = {user1 : jsonData,
+user2: jsonData2}
+
+console.log(users);
 var output = document.getElementById('output');
 
-output.innerHTML = jsonData.firstName + ' ' + jsonData.lastName + ' ' + jsonData.phoneNumbers[1].type;
+output.innerHTML = users.user2.firstName + ' ' + users.user2.lastName + ' ' + users.user2.phoneNumbers[1].type + ' ' + users.user2.address.city;
 
